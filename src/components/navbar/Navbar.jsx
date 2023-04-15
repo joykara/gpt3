50fde6015f7ae7e61d8e3fccf9e8ab7fdbde20a7
joykara <joykarani1@gmail.com>
2023-04-15 20:51:03 +0300

    navbar jsx & css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import logo from '../../assets/logo.svg'
+import logo from '../../assets/logo.svg';
 import './navbar.css';
 
 const Menu = () => (
@@ -14,42 +14,45 @@ const Menu = () => (
 )
 
 //BEM: Block Element Modifier
+
 const Navbar = () => {
-  const { toggleMenu, setToggleMenu } = useState(false)
+  const [toggleMenu, setToggleMenu] = useState(false);
 
   return (
-    <div className='gpt3__navbar'>
-      <div className='gpt3__navbar-links'>
-        <div className='gpt3__navbar-links_logo'>
-          <img src={logo} alt="logo" />
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} />
         </div>
         <div className="gpt3__navbar-links_container">
-            <Menu />
+          <Menu />
         </div>
       </div>
+
       <div className="gpt3__navbar-sign">
         <p>Sign in</p>
-        <button type='button'>Sign up</button>
+        <button type="button">Sign up</button>
       </div>
+
       <div className="gpt3__navbar-menu">
-        { toggleMenu
-          ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
-          :<RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
         }
         {toggleMenu && (
-          <div className="gpt__navbar-menu_container scale-up-center">
-            <div className="gpt3__navbar-menu_container-links">
-              <Menu />
-              <div className="gpt3__navbar-menu_container-links-sign">
-                <p>Sign in</p>
-                <button type='button'>Sign up</button>
-              </div>
-            </div>
+        <div className="gpt3__navbar-menu_container scale-up-center">
+          <div className="gpt3__navbar-menu_container-links">
+            <Menu />
           </div>
+          <div className="gpt3__navbar-menu_container-links-sign">
+            <p>Sign in</p>
+            <button type="button">Sign up</button>
+          </div>
+        </div>
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
