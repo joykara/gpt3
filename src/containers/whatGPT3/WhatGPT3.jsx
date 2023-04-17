313c40f3699e7b791ea65753909676ed1d624ffe
joykara <joykarani1@gmail.com>
2023-04-17 18:36:40 +0300

    edit

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -6,7 +6,7 @@ const WhatGPT3 = () => {
   return (
     <div className='gpt3__whatgpt3 section__margin'>
       <div className="gpt3__whatgpt3-feature">
-      <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+      <Feature title="What is GPT-3" text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati porro, quo provident officiis consequuntur autem repellat libero itaque voluptatem enim tenetur vero, id impedit reprehenderit. Enim quia recusandae modi quisquam!" />
       </div>
 
     <div className="gpt3__whatgpt3-heading">
@@ -15,9 +15,9 @@ const WhatGPT3 = () => {
       </div>
 
     <div className="gpt3__whatgpt3-container">
-      <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
-      <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
-      <Feature title="Education" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b" />
+      <Feature title="Chatbots" text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati porro, quo provident officiis consequuntur autem repellat " />
+      <Feature title="Knowledgebase" text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati porro," />
+      <Feature title="Education" text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati porro, quo provident officiis consequuntur autem " />
     </div>
     
   </div>
